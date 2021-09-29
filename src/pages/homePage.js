dd3fd8325b1643162ea5cf7785d05096617ed36d
Ben Clevenger <benhcleve@gmail.com>
2021-09-29 16:02:16 -0400

    added carousel to homepage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import HomeButtons from "../components/homeButtons";
 
@@ -7,11 +7,37 @@ const HomePage = () => {
   return (
     <>
       <HomeButtons />
-      <Container fluid className="vh-100" style={{ backgroundColor: "black" }}>
+      <Container fluid className="vh-100 " style={{ backgroundColor: "black" }}>
         <Row className=" align-items-center">
           <div>
             <h1 style={{ textAlign: "center", backgroundColor: "white" }}>Home</h1>
           </div>
+
+          <Carousel fluid style={{ width: "50%", margin: "auto" }}>
+            <Carousel.Item>
+              <img className="d-block w-100" src=" " alt="First slide" />
+              <Carousel.Caption>
+                <h3>Header 1</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src=" " alt="Second slide" />
+
+              <Carousel.Caption>
+                <h3>Header 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src=" " alt="Third slide" />
+
+              <Carousel.Caption>
+                <h3>Header 3</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Row>
       </Container>
       ;
